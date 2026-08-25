ee7f3f18fa065b80398e0d99b19c4dab11b91f9e
evrstt <billycruse@gmail.com>
2026-08-24 23:57:54 -0500

    Initial Commit from Unity creation + PlayerControllerV1
    - Initial commit after unity creation
    - added player controller with simple left/right movement.

---
+++ Assets/Scripts/PlayerControllerV1.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerV1 : MonoBehaviour
+{
+[SerializeField] private float moveSpeed = 10f;
+
+[SerializeField] private Transform groundCheck;
+[SerializeField] private float groundCheckRadius = 0.2f;
+[SerializeField] private LayerMask groundLayer;
+
+private Rigidbody2D rb;
+
+private Vector2 moveInput;
+private bool isGrounded;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
