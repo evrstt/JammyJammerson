f0397199c63d3903cf372ffb14cac62d80383988
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-08-28 13:43:04 -0500

    updated movingplatform.cs
    still need to test and do values in inspector etc

--- Assets/Scripts/Environment/MovingPlatform.cs
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -3,19 +3,26 @@ using UnityEngine;
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private float speed;
-    [SerializeField] private Transform points;
+    [SerializeField] private Transform[] points;
     [SerializeField] private int startingPoint;
     private int pointIndex;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = points[startingPoint].position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(transform.position, points[pointIndex].position) < 0.02f)
+        {
+            pointIndex++;
+            if (pointIndex == points.Length)
+            {
+                pointIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, points[pointIndex].position, speed * Time.deltaTime);
     }
 }
