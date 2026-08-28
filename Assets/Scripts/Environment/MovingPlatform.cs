0b3f05a3b2d4cedf24a670c8bcd4a51a862a05a2
gabby-reidy <gabrielle.reidy@g.austincc.edu>
2026-08-28 12:33:25 -0500

    Added movingplatform.cs

---
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform points;
+    [SerializeField] private int startingPoint;
+    private int pointIndex;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
